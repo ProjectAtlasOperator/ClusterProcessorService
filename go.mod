b3dcf1857fe1e614136fb72af6b33dde36bf31f8
Yevhenii Kozhevin <yevhenii.kozhevin@student.upjs.sk>
2020-10-20 13:34:44 +0200

    c

--- go.mod
+++ go.mod
@@ -10,6 +10,10 @@ require (
 	github.com/gobuffalo/mw-i18n v1.1.0
 	github.com/gobuffalo/mw-paramlogger v1.0.0
 	github.com/gobuffalo/packr/v2 v2.8.0
+	github.com/gobuffalo/suite v2.8.2+incompatible
 	github.com/unrolled/secure v1.0.8
-	k8s.io/client-go v0.19.0 // indirect
+	k8s.io/api v0.19.2
+	//k8s.io/apimachinery v0.19.2
+	k8s.io/client-go v0.0.0-20201007120136-5682372f3538
+	k8s.io/api/apiserverinternal/v1alpha1 v0.0.0-20201005155906-2c3c141c931c
 )
--- main.go
+++ main.go
@@ -1,38 +1,56 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"log"
 
 	"cluster_processor_service/actions"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
-// main is the starting point for your Buffalo application.
-// You can feel free and add to this `main` method, change
-// what it does, etc...
-// All we ask is that, at some point, you make sure to
-// call `app.Serve()`, unless you don't want to start your
-// application that is. :)
 func main() {
 	app := actions.App()
 	if err := app.Serve(); err != nil {
 		log.Fatal(err)
 	}
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	for {
+		// get pods in all the namespaces by omitting namespace
+		// Or specify namespace to get pods in particular namespace
+		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+		_, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			fmt.Printf("Pod example-xxxxx not found in default namespace\n")
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+		} else if err != nil {
+			panic(err.Error())
+		} else {
+			fmt.Printf("Found example-xxxxx pod in default namespace\n")
+		}
+		time.Sleep(10 * time.Second)
+	}
 }
 
-/*
-# Notes about `main.go`
-
-## SSL Support
-
-We recommend placing your application behind a proxy, such as
-Apache or Nginx and letting them do the SSL heavy lifting
-for you. https://gobuffalo.io/en/docs/proxy
-
-## Buffalo Build
-
-When `buffalo build` is run to compile your binary, this `main`
-function will be at the heart of that binary. It is expected
-that your `main` function will start your application using
-the `app.Serve()` method.
-
-*/
