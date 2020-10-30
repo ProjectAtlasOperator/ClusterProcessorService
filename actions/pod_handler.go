dc4cb76e264e327262e08cfda3f3dd27e41317b0
Pavol Szeles <pavol.szeles@student.tuke.sk>
2020-10-30 21:24:48 +0100

    show pod information in /podinfo root

--- actions/pod_handler.go
+++ actions/pod_handler.go
@@ -7,6 +7,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	//"k8s.io/client-go/metadata"
 	"net/http"
 	"time"
 
@@ -29,15 +30,20 @@ func PodInfoHander(c buffalo.Context) error {
 		// get pods in all the namespaces by omitting namespace
 		// Or specify namespace to get pods in particular namespace
 		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+
 		if err != nil {
 			panic(err.Error())
 		}
 		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+		c.Set("pods", pods)
+
+		//return c.Render(http.StatusOK, r.HTML("index.html"))
 
 		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
 		_, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
 		if errors.IsNotFound(err) {
 			fmt.Printf("Pod example-xxxxx not found in default namespace\n")
+			fmt.Printf("hello there")
 		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
 			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
 		} else if err != nil {
@@ -45,10 +51,9 @@ func PodInfoHander(c buffalo.Context) error {
 		} else {
 			fmt.Printf("Found example-xxxxx pod in default namespace\n")
 		}
+		break
 		time.Sleep(10 * time.Second)
 	}
 
-	return c.Render(http.StatusOK, r.String(http.StatusText(200)))
+	return c.Render(http.StatusOK, r.HTML("pod-metadata.html"))
 }
-
-
